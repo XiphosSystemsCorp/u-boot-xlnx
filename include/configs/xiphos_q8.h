d604dfb2324fef59113d9d0c3693086f628b5b82
Liam Beguin <lvb@xiphos.com>
2017-08-30 10:51:36 -0400

    board: xiphos: add initial support for Q8 boards
    Add initial support for Xiphos Q8 board family.
    These boards are based on Xilinx zynqmp processors.
    
    reference files:
     - board/xilinx/zynqmp/*                       -> board/xiphos/q8/*
     - include/configs/xilinx_zynqmp_zcu102.h      -> include/configs/xiphos_q8.h
     - configs/xilinx_zynqmp_zcu102_revB_defconfig -> configs/xiphos_q8_defconfig
    
    Signed-off-by: Liam Beguin <lvb@xiphos.com>

---
+++ include/configs/xiphos_q8.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017, Xiphos System Corp.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ * Based in part on:
+ *   include/configs/xilinx_zynqmp_zcu102.h
+ */
+
+#ifndef __CONFIG_XIPHOS_Q8_H
+#define __CONFIG_XIPHOS_Q8_H
+
+#include <configs/xilinx_zynqmp.h>
+
+#define CONFIG_MTD_PARTITIONS
+
+/* MTD partition settings */
+#define MTDIDS_DEFAULT		"nor0=flash"
+#define MTDPARTS_DEFAULT	"mtdparts=flash:" \
+				"128k(qspi0-nom-xscinfo)," \
+				"10m(qspi0-nom-fw)," \
+				"54m(qspi0-nom-rootfs)," \
+				"128k(qspi0-gold-xscinfo)," \
+				"10m(qspi0-gold-fw)," \
+				"-(qspi0-gold-rootfs)"
+
+/* U-Boot env settings: ENV_IS_IN_UBIFS not yet supported */
+#undef CONFIG_ENV_IS_IN_FAT
+
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootcmd=echo; echo ' ** WARNING: USING DEFAULT U-BOOT ENVIRONMENT **'; echo\0"
+
+#endif /* __CONFIG_XIPHOS_Q8_H */
